fb2a80f65d36a234519035c6a303610332f55604
Michal Stawarski <m.ski@poczta.fm>
2020-09-27 14:12:16 +0200

    init commit

---
+++ src/io/codeswarm/BitwisePartOne.java
@@ -0,0 +1,56 @@
+package io.codeswarm;
+
+public class BitwisePartOne {
+
+    //
+    // AND OPERATOR &
+    //
+
+    // Bitwise AND (&) operator checks if is number even
+    public static boolean checkIfEven(int number) {
+        return (number & 1) == 0;
+    }
+
+    // Bitwise AND (&) operator checks if is number odd
+    public static boolean checkIfOdd(int number) {
+        return (number & 1) == 1;
+    }
+
+    //
+    // LEFT SHIFT OPERATOR <<
+    //
+
+    // Left shift operator is equivalent to multiplication by 2 (<< 1)
+    public static int multiplyByTwo(int number) {
+        return number << 1;
+    }
+
+    // Left shift operator is equivalent to multiplication by 4 (<< 2)
+    public static int multiplyByFour(int number) {
+        return number << 2;
+    }
+
+    // Left shift operator is equivalent to multiplication by factor (<< factor * 2)
+    public static int multiplyByFactor(int number, int factor) {
+        return number << factor;
+    }
+
+    //
+    // RIGHT SHIFT OPERATOR >>
+    //
+
+    // Right shift operator (>>) here is equivalent to division by 2 (>> 1)
+    public static int divideByTwo(int number) {
+        return number >> 1;
+    }
+
+    // Right shift operator (>>) here is equivalent to division by 4 (>> 2)
+    public static int divideByFour(int number) {
+        return number >> 2;
+    }
+
+    // Right shift operator (>>) here is equivalent to division by factor (>> factor / 2)
+    public static int divideByFactor(int number, int factor) {
+        return number >> factor;
+    }
+}
